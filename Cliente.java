7adac07de660344f92cda848a599c2e808af2f86
ivette.lara <ivette.lara@mh.gob.sv>
2017-07-19 12:56:27 -0600

    primer commit

---
+++ Cliente.java
@@ -0,0 +1,37 @@
+package data;
+
+/**
+ *
+ * @author ivette.lara
+ */
+public class Cliente {
+    private int idCliente;
+    private String nombreCliente;
+    private String nit;
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+    
+    
+}
